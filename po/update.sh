810a27fd81c9556f159f8afe031caa72252ed70f
Fatih Demir <kabalak@src.gnome.org>
2000-02-25 14:50:28 +0000

    Some modifications of the ./configure mechanism ..

--- po/update.sh
+++ po/update.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-
+#######################################################################
+echo "Building the gtranslator.pot ..." 
 xgettext --default-domain=gtranslator --directory=.. \
   --add-comments --keyword=_ --keyword=N_ \
   --files-from=./POTFILES.in \
 && test ! -f gtranslator.po \
    || ( rm -f ./gtranslator.pot \
     && mv gtranslator.po ./gtranslator.pot )
+#######################################################################
