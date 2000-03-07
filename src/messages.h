36e1e33e1f87719ed566921cf706094c64a1be6d
Fatih Demir <kabalak@src.gnome.org>
2000-03-07 11:41:08 +0000

    Added a messages header

---
+++ src/messages.h
@@ -0,0 +1,41 @@
+/**
+ * Fatih Demir [ kabalak@gmx.net ]
+ * 
+ * (C) 2000 Published under GNU GPL V 2.0+
+ *
+ * The messages-general stuff will be stored 
+ * here .
+ **/
+
+#include "msg_db.h"
+#include "callbacks.h"
+#include "header_stuff.h"
+
+/**
+ * The structure with general 
+ *  informations ...
+ **/
+struct po_file 
+{
+	char *po_filename;
+	unsigned int file_length;
+	gboolean opened;
+};
+
+/**
+ * Every message should be filled
+ *  in here .
+ **/
+struct message
+{
+	struct po_file *po;
+	char *msgid;
+	char *msgstr;
+	unsigned int position;
+	gboolean translated;
+};
+
+/**
+ * The used message structure pointer 
+ **/
+struct message *msg;
