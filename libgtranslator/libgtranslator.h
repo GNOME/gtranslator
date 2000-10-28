871fcb2d46dfe3dcb3e7967ac4581b8811d509b5
Fatih Demir <kabalak@src.gnome.org>
2000-10-28 18:07:03 +0000

    Added a new central header for libgtranslator

---
+++ libgtranslator/libgtranslator.h
@@ -0,0 +1,34 @@
+/**
+*
+* (C) 2000 Fatih Demir -- kabalak / kabalak@gmx.net
+*
+* This is distributed under the GNU GPL V 2.0 or higher which can be
+*  found in the file COPYING for further studies.
+*
+* Enjoy this piece of software, brain-crack and other nice things.
+*
+* WARNING: Trying to decode the source-code may be hazardous for all your
+*	future development in direction to better IQ-Test rankings!
+*
+**/
+
+#ifndef LIBGTRANSLATOR_H
+#define LIBGTRANSLATOR_H 1
+
+/**
+* The central header for libgtranslator.
+**/
+
+/**
+* Include all the other headers.
+**/
+#include <libgtranslator/gtranslatord.h>
+#include <libgtranslator/gtranslator-preferences-dialog.h>
+#include <libgtranslator/handle-db.h>
+#include <libgtranslator/parse-db.h>
+#include <libgtranslator/preferences.h>
+#include <libgtranslator/stylistics.h>
+#include <libgtranslator/team-handle.h>
+#include <libgtranslator/translation-database.h>
+
+#endif
