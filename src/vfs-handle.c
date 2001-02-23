204534466f62008fddd5336e07169c43838ac223
Fatih Demir <kabalak@src.gnome.org>
2001-02-23 15:38:52 +0000

    copyright headers & comment fixes; there's no libgtranslator anylonger

--- src/vfs-handle.c
+++ src/vfs-handle.c
@@ -1,12 +1,12 @@
 /*
  * (C) 2000-2001 	Fatih Demir <kabalak@gtranslator.org>
  *
- * libgtranslator is free software; you can redistribute it and/or modify
+ * gtranslator is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License, or
  *    (at your option) any later version.
  *
- * libgtranslator is distributed in the hope that it will be useful,
+ * gtranslator is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *    GNU General Public License for more details.
