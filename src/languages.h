3ff11446a63b1f0c8b56b15e8a52507719c21b6e
Fatih Demir <kabalak@src.gnome.org>
2001-06-18 14:28:58 +0000

    Some further updates..

--- src/languages.h
+++ src/languages.h
@@ -1,5 +1,6 @@
 /*
  * (C) 2000-2001 	Fatih Demir <kabalak@gtranslator.org>
+ *			GNOME I18N List <gnome-i18n@gnome.org>
  *
  * gtranslator is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -28,71 +29,68 @@
  */
 typedef struct {
 	/*
-	 * 1 - The name of the language 
+	 * 1 - The name of the language (e.g. "Turkish").
 	 */
 	gchar *name;
 	
 	/*
-	 * 2 - The language-code 
+	 * 2 - The language-code (e.g. "tr").
 	 */
 	gchar *lcode;
 	
 	/*
-	 * 3 - The encoding preferably used for this language 
+	 * 3 - The encoding preferably used for this language (e.g. "iso-8859-9").
 	 */
 	gchar *enc;
 	
 	/* 
-	 * 4 - The group's( at LI/somewhere else ) EMail for the language 
+	 * 4 - The group's EMail list (e.g. "gnome-turk@gnome.org").
 	 */
 	gchar *group;
 	
 	/*
-	 * 5 - The transfer bit count for the language 
+	 * 5 - The transfer bit count for the language (e.g. "8bit"
 	 */
 	gchar *bits;
 } GtrLanguage;
 
 /*
- * Nothing changed here, besides that we've switched to the structure
- *  instead of the gchar*-array ..
- *
- * NOTE : I had to use the N() macro as the table must be constant ...
+ * NOTE : I had to use the N() macro as the table must be constant...
  *	  but in the Prefs-box and in the other places where they appear 
- *	   translated because of a gettext(S)-call ...
+ *	   translated because of a gettext()-calls...
  */
 static GtrLanguage languages[] = {
 	{
 	 N_("Afrikaans"), "af", "iso-8859-1",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Arabic"), "ar", "iso-8859-6",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Azerbaijani Turkish"), "az", "utf-8",
-	 "", "8bit"		
+	 "linuxaz@azerimal.net", "8bit"
 	},
 	{
 	 N_("Basque"), "eu", "iso-8859-1",
-	 "linux-eu@chanae.alphanet.ch", "8bit"		
+	 "linux-eu@chanae.alphanet.ch", "8bit"
 	},
 	{
 	 N_("Bielorussian"), "be", "windows-1251",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Britton"), "br", "iso-8859-1",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Bulgarian"), "bg", "windows-1251",
-	 "bg@li.org", "8bit"		
+	 "bg@li.org", "8bit"
 	},
 	{
 	 N_("Catalan"), "ca", "iso-8859-1",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Chinese/Simplified"), "zh_CN.GB2312", "GB2312",
@@ -100,19 +98,19 @@ static GtrLanguage languages[] = {
 	},
 	{
 	 N_("Chinese/Traditional"), "zh_TW.Big5", "Big5",
-	 "zh-l10n@linux.org.tw", "8bit"		
+	 "zh-l10n@linux.org.tw", "8bit"
 	},
 	{
 	 N_("Croatian"), "hr", "iso-8859-2",
-	 "lokalizacija@linux.hr", "8bit"		
+	 "lokalizacija@linux.hr", "8bit"
 	},
 	{
 	 N_("Czech"), "cs", "iso-8859-2",
-	 "cs@li.org", "8bit"		
+	 "cs@li.org", "8bit"
 	},
 	{
 	 N_("Danish"), "da", "iso-8859-1",
-	 "da@li.org", "8bit"		
+	 "da@li.org", "8bit"
 	},
 	{
 	 N_("Dutch"), "nl", "iso-8859-15",
@@ -120,115 +118,115 @@ static GtrLanguage languages[] = {
 	},
 	{
 	 N_("English"), "en", "iso-8859-1",
-	 "en@li.org", "8bit"		
+	 "en@li.org", "8bit"
 	},
 	{
 	 N_("English/Canada"), "en_CA", "iso-8859-1",
-	 "en@li.org", "8bit"		
+	 "en@li.org", "8bit"
 	},
 	{
 	 N_("English/GB"), "en_GB", "iso-8859-1",
-	 "en@li.org", "8bit"		
+	 "en@li.org", "8bit"
 	},
 	{
 	 N_("English/USA"), "en", "iso-8859-1",
-	 "en@li.org", "8bit"		
+	 "en@li.org", "8bit"
 	},
 	{
 	 N_("Estonian"), "et", "iso-8859-15",
-	 "et@li.org", "8bit"		
+	 "et@li.org", "8bit"
 	},
 	{
 	 N_("Farsi"), "fa", "isiri-3342",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Finnish"), "fi", "iso-8859-15",
-	 "fi@li.org", "8bit"		
+	 "fi@li.org", "8bit"
 	},
 	{
 	 N_("French"), "fr", "iso-8859-1",
-	 "fr@li.org", "8bit"		
+	 "fr@li.org", "8bit"
 	},
 	{
 	 N_("Galician"), "gl", "iso-8859-1",
-	 "trasno@ceu.fi.udc.es", "8bit"		
+	 "trasno@ceu.fi.udc.es", "8bit"
 	},
 	{
 	 N_("German"), "de", "iso-8859-1",
-	 "gnome-de@gnome.org", "8bit"		
+	 "gnome-de@gnome.org", "8bit"
 	},
 	{
 	 N_("Greek"), "el", "iso-8859-7",
-	 "nls@tux.hellug.gr", "8bit"		
+	 "nls@tux.hellug.gr", "8bit"
 	},
 	{
 	 N_("Hungarian"), "hu", "iso-8859-2",
-	 "", "8bit"		
+	 "hu@gnome.hu", "8bit"
 	},
 	{
 	 N_("Indonesian"), "id", "iso-8859-1",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Italian"), "it", "iso-8859-1",
-	 "it@li.org", "8bit"		
+	 "it@li.org", "8bit"
 	},
 	{
 	 N_("Irish"), "ga", "iso-8859-1",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Japanese"), "ja", "euc-jp",
-	 "translation@gnome.gr.jp", "8bit"		
+	 "translation@gnome.gr.jp", "8bit"
 	},
 	{
 	 N_("Korean"), "ko", "euc-kr",
-	 "", "8bit"		
+	 "ko@li.org", "8bit"
 	},
 	{
 	 N_("Latvian"), "lv", "iso-8859-13",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Lithuanian"), "lt", "iso-8859-13",
-	 "komp_lt@konferencijos.lt", "8bit"		
+	 "komp_lt@konferencijos.lt", "8bit"
 	},
 	{
 	 N_("Norwegian/Bokmaal"), "no", "iso-8859-1",
-	 "no@li.org", "8bit"		
+	 "no@li.org", "8bit"
 	},
 	{
 	 N_("Norwegian/Nynorsk"), "nn", "iso-8859-1",
-	 "no@li.org", "8bit"		
+	 "no@li.org", "8bit"
 	},
 	{
 	 N_("Polish"), "pl", "iso-8859-2",
-	 "gnomepl@pandora.info.bielsko.pl", "8bit"		
+	 "gnomepl@pandora.info.bielsko.pl", "8bit"
 	},
 	{
 	 N_("Portuguese"), "pt", "iso-8859-1",
-	 "pt@li.org", "8bit"		
+	 "pt@li.org", "8bit"
 	},
 	{
 	 N_("Portuguese/Brazil"), "pt_BR", "iso-8859-1",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Romanian"), "ro", "iso-8859-2",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Russian"), "ru", "koi8-r",
-	 "ru@li.org", "8bit"		
+	 "ru@li.org", "8bit"
 	},
 	{
 	 N_("Serbian/Cyrillic"), "sp", "iso-8859-5",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Serbian/Latin"), "sr", "iso-8859-2",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Slovak"), "sk", "iso-8859-2",
@@ -236,55 +234,55 @@ static GtrLanguage languages[] = {
 	},
 	{
 	 N_("Slovenian"), "sl", "iso-8859-2",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Spanish"), "es", "iso-8859-1",
-	 "es@li.org", "8bit"		
+	 "es@li.org", "8bit"
 	},
 	{
 	 N_("Spanish/Spain"), "es_ES", "iso-8859-1",
-	 "es@li.org", "8bit"		
+	 "es@li.org", "8bit"
 	},
 	{
 	 N_("Spanish/Mexico"), "es_MX", "iso-8859-1",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Swedish"), "sv", "iso-8859-1",
-	 "sv@li.org", "8bit"		
+	 "sv@li.org", "8bit"
 	},
 	{
 	 N_("Tamil"), "ta", "tscii",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Thai"), "th", "tis-620",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Turkish"), "tr", "iso-8859-9",
-	 "gnome-turk@gnome.org", "8bit"	
+	 "gnome-turk@gnome.org", "8bit"
 	},
 	{
 	 N_("Ukrainian"), "uk", "koi8-u",
-	 "linux@linux.org.ua", "8bit"		
+	 "linux@linux.org.ua", "8bit"
 	},
 	{
 	 N_("Uzbekian"), "uz", "iso-8859-1",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Vietnamese"), "vi", "tcvn-5712",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	{
 	 N_("Walloon"), "wa", "iso-8859-1",
-	 "linux-wa@chanae.alphanet.ch", "8bit"		
+	 "linux-wa@chanae.alphanet.ch", "8bit"
 	},
 	{
 	 N_("Welsh"), "cy", "iso-8859-14",
-	 "", "8bit"		
+	 "", "8bit"
 	},
 	/*
          * This structures-list has to be NULL-terminated
