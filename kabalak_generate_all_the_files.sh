ba3f45c31a52a9b53f0cf536966b343f2b03c534
Fatih Demir <kabalak@src.gnome.org>
2001-08-12 14:36:16 +0000

    plain text files updates, corrections, nautilus view removal -- part IV

--- kabalak_generate_all_the_files.sh
+++ kabalak_generate_all_the_files.sh
@@ -57,6 +57,6 @@ while [ $t -gt 1 ]
 	t=$[ $t -1 ]
 	done
 $a " last chance (1) " && sleep 1 && echo ""
-./configure --enable-debug --enable-nautilus-view --enable-mime-bind=yes \
+./configure --enable-debug \
 	"$@" && echo -e "\n\tHappy $package-ing\n" && exit 0
 #######################################################################
