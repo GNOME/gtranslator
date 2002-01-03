d8af879fb8c94f9142b605bbaab2192207ffe040
Fatih Demir <kabalak@src.gnome.org>
2002-01-03 20:46:15 +0000

    Updated scripts.

--- data/scripts/cleanup-gtranslator-settings.sh
+++ data/scripts/cleanup-gtranslator-settings.sh
@@ -6,13 +6,14 @@ echo "Closing down gtranslator, gconf and deleting all setting files"
 echo " of gtranslator..."
 echo "---------------------------------------------------------------"
 
-killall gtranslator 2>&1 >/dev/null
-gconftool --shutdown 2>&1 >/dev/null
+killall gtranslator 2>&1 >/dev/null && sleep 3 && \
+gconftool --shutdown 2>&1 >/dev/null && sleep 3 && \
 rm -rf ~/.gtranslator ~/.gconf/apps/gtranslator \
 	~/.gnome/gtranslator ~/.gnome/accels/gtranslator
 
 echo "---------------------------------------------------------------"
-echo "Hopefully done!"
+echo "Hopefully done! All of your gtranslator settings have been"
+echo " deleted."
 echo "---------------------------------------------------------------"
 
 exit 0
