0817bf5c510ef7135d6029bd60fd69aef79d10a7
Fatih Demir <kabalak@src.gnome.org>
2002-01-13 16:12:16 +0000

    Added ThinIce colorscheme + updated scripts..

--- data/scripts/cleanup-gtranslator-settings.sh
+++ data/scripts/cleanup-gtranslator-settings.sh
@@ -1,19 +1,43 @@
 :
 #!/bin/sh
+#
+# (C) 2001-2002 Fatih Demir <kabalak@gtranslator.org>
+#
+# cleanup-gtranslator-settings.sh -- Cleans up gtranslator's settings and
+#  "personal data" directories for the user running this script -- it simply
+#    removes all of the corresponding files in the user's home directory.
+#
+
+remove_commands ()  {
+	killall gtranslator 2>&1 >/dev/null ; sleep 3 && \
+	gconftool --shutdown 2>&1 >/dev/null && sleep 3 && \
+	rm -rf ~/.gtranslator ~/.gconf/apps/gtranslator \
+		~/.gnome/gtranslator ~/.gnome/accels/gtranslator
+
+	echo "---------------------------------------------------------------"
+	echo "All of your gtranslator settings have been (hopefully) deleted!"
+	echo "---------------------------------------------------------------"
+}
 
 echo "---------------------------------------------------------------"
 echo "Closing down gtranslator, gconf and deleting all setting files"
 echo " of gtranslator..."
 echo "---------------------------------------------------------------"
-
-killall gtranslator 2>&1 >/dev/null && sleep 3 && \
-gconftool --shutdown 2>&1 >/dev/null && sleep 3 && \
-rm -rf ~/.gtranslator ~/.gconf/apps/gtranslator \
-	~/.gnome/gtranslator ~/.gnome/accels/gtranslator
-
+echo "Are you really sure to delete all person. settings/data created"
+echo " by gtranslator?"
 echo "---------------------------------------------------------------"
-echo "Hopefully done! All of your gtranslator settings have been"
-echo " deleted."
-echo "---------------------------------------------------------------"
+read -p "[y/N]? " answer
+
+case $answer in
+[yY]*|[zZ]*)
+	remove_commands	
+;;
+*)
+	echo "---------------------------------------------------------------"
+	echo "Wise answer :-) Your settings are not removed/cleaned up now..."
+	echo "---------------------------------------------------------------"
+		exit 1
+;;
+esac
 
 exit 0
