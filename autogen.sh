83c4cf42c22ab4eb6a808e710b9af12774d12179
Fatih Demir <kabalak@src.gnome.org>
2002-07-20 15:21:37 +0000

    Adept it, yeaaaaaaah

--- autogen.sh
+++ autogen.sh
@@ -4,18 +4,41 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="GTRANSLATOR"
+PKG_NAME="gtranslator"
 
 (test -f $srcdir/configure.in \
-  && test -f $srcdir/HACKING \
-  && test -d $srcdir/src) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
+  && test -d $srcdir/src \
+  && test -f $srcdir/src/main.c) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" is NOT the top source"
+    echo " tree level of the gtranslator directories! Oooppsia!"
     exit 1
 }
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
-    exit 1
-}
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
+if libtool --version >/dev/null 2>&1; then
+    vers=`libtool --version | sed -e "s/^[^0-9]*//" -e "s/ .*$//" | awk 'BEGIN { FS = "."; } { printf "%d", ($1 * 1000 + $2) * 1000 + $3;}'`
+    if test "$vers" -ge 1003003; then
+        true
+    else
+        echo "Please upgrade your libtool to version 1.3.3 or better." 1>&2
+        exit 1
+    fi
+fi
+
+ifs_save="$IFS"; IFS=":"
+for dir in $PATH ; do
+  test -z "$dir" && dir=.
+  if test -f $dir/gnome-autogen.sh ; then
+    gnome_autogen="$dir/gnome-autogen.sh"
+    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
+    break
+  fi
+done
+IFS="$ifs_save"
+
+if test -z "$gnome_autogen" ; then
+  echo "You need to install the gnome-common module and make"
+  echo "sure the gnome-autogen.sh script is in your \$PATH."
+  exit 1
+fi
+
+GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 . $gnome_autogen
