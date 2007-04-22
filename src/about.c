befb041ab87ad41a4a752ba1a837db3ffb860945
Ignacio Casal Quinteiro <icq@src.gnome.org>
2007-04-22 22:23:46 +0000

    Removed libgnome dependencies, updated and added license.
    svn path=/trunk/; revision=3058

--- src/about.c
+++ src/about.c
@@ -1,7 +1,8 @@
 /*
- * (C) 2000-2003 	Fatih Demir <kabalak@kabalak.net>
+ * (C) 2000-2007 	Fatih Demir <kabalak@kabalak.net>
  *			Gediminas Paulauskas <menesis@kabalak.net>
  *			Roy-Magne Mo <rmo@sunnmore.net>
+ *			Ignacio Casal <nacho.resa@gmail.com>
  * 
  * gtranslator is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -24,19 +25,20 @@
 #endif
 
 #include "about.h"
+#include "gui.h"
 
 #include <string.h>
 
-#include <libgnome/gnome-i18n.h>
-#include <libgnome/gnome-program.h>
+#include <glib/gi18n.h>
 
-#include <gtk/gtk.h>
+#include <gtk/gtkaboutdialog.h>
 
 /*
  * Creates and shows the about box for gtranslator.
  */ 
 void gtranslator_about_dialog(GtkWidget * widget, gpointer useless)
 {
+	gchar *license_trans;
 	
 	const gchar *authors[] = {
 		"Ross Golder <ross@golder.org>",
@@ -53,38 +55,44 @@ void gtranslator_about_dialog(GtkWidget * widget, gpointer useless)
 		"Emese Kovacs <emese@gnome.hu>",
 		NULL
 	};	
-
-	GdkPixbuf *pixbuf = NULL;
-	gchar *image = gnome_program_locate_file(NULL, GNOME_FILE_DOMAIN_PIXMAP, "gtranslator.png",TRUE, NULL);
-	GdkPixbuf *file = gdk_pixbuf_new_from_file (image, NULL);
-	GtkWidget *about = g_object_new (GTK_TYPE_ABOUT_DIALOG,
-                                     "name", PACKAGE_NAME, 
-                                     "version", VERSION,
-			             "copyright", _("(C) 1999-2005 The Free Software Foundation, Inc."),
-				     "comments", _("gtranslator is a po file editing suite with many bells and whistles."),
-			             "authors", authors,
-			             "documenters", documenters,
-			             "translator-credits", _("translator-credits"),
-				     "logo", file,
-                                     NULL);
-	if (about != NULL)
-	{
-		gtk_window_present(GTK_WINDOW(about));
-		return;
-	}
-
-	if (file) {
-		pixbuf = gdk_pixbuf_new_from_file (image, NULL);
-		g_free (file);
-	} else
-		g_warning (G_STRLOC ": gtranslator.png cannot be found");
-
-	if (pixbuf) {
-		g_object_unref (pixbuf);
-	}
 	
-	g_signal_connect (G_OBJECT (about), "destroy",
-			  G_CALLBACK (gtk_widget_destroyed), &about);
 	
-	gtk_widget_show (about);
+	const char *license[] = {
+		N_("gtranslator is free software; you can redistribute it and/or modify "
+		"it under the terms of the GNU General Public License as published by "
+		"the Free Software Foundation; either version 2 of the License, or "
+		"(at your option) any later version."),
+		N_("gtranslator is distributed in the hope that it will be useful, "
+		"but WITHOUT ANY WARRANTY; without even the implied warranty of "
+		"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+		"GNU General Public License for more details."),
+		N_("You should have received a copy of the GNU General Public License "
+		"along with Sound Juicer; if not, write to the Free Software Foundation,"
+		"59 Temple Place, Suite 330, Boston, MA  02111-1307  USA")
+	};
+
+
+	license_trans = g_strconcat (_(license[0]), "\n\n",
+	                             _(license[1]), "\n\n",
+         	                     _(license[2]), "\n\n",
+                	             NULL);                          
+
+	
+	gtk_show_about_dialog (GTK_WINDOW (gtranslator_application),
+                         "comments", _("gtranslator is a po file editing suite with many bells and whistles."),
+                         "version", VERSION,
+                         "copyright", _("(C) 1999-2007 The Free Software Foundation, Inc."),
+                         "authors", authors,
+                         "documenters", documenters,
+                         /*
+                          * Note to translators: put here your name and email so it will show
+                          * up in the "about" box
+                          */
+                         "translator-credits", _("translator-credits"),
+                         "logo-icon-name", "gtranslator",
+                         "license", license_trans,
+                         "wrap-license", TRUE,
+                         NULL);
+
+	g_free (license_trans);
 }
