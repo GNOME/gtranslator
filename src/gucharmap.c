187914f911ef443e155ad512e79ed26de0a0728b
Ignacio Casal Quinteiro <nacho.resa@gmail.com>
2007-07-24 14:05:31 +0000

    Added charmap.ch, gucharmap.ch Added for manage the gucharmap integration.
    2007-07-24  Ignacio Casal Quinteiro  <nacho.resa@gmail.com>
    
            * Makefile.am: Added charmap.ch, gucharmap.ch
            * gucharmap.ch: Added for manage the gucharmap integration.
    
    svn path=/trunk/; revision=3164

---
+++ src/gucharmap.c
@@ -0,0 +1,18 @@
+#include "gui.h"
+#include "message-area.h"
+
+#define GLADE_GUCHARMAP_BOX "gucharmap_box"
+
+void
+gtranslator_gucharmap_create()
+{
+	/*GtkWidget *gucharmap;
+	GtkWidget *gucharmap_box;
+
+	gucharmap = gtranslator_message_area_new();
+	gucharmap_box = glade_xml_get_widget(glade, GLADE_GUCHARMAP_BOX);
+	
+	gtk_box_pack_start(GTK_BOX(gucharmap_box), gucharmap, TRUE, TRUE, 0);
+	
+	gtk_widget_show(gucharmap);*/
+}
---
+++ src/gucharmap.h
@@ -0,0 +1,7 @@
+#ifndef __GTR_GUCHARMAP__
+#define __GTR_GUCHARMAP__
+
+void gtranslator_gucharmap_create();
+
+
+#endif
