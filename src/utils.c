038f3fb77b5a32d2e91a798f0a1509a49b340275
Pablo Sanxiao Roca <psanxiao@src.gnome.org>
2008-09-23 11:05:08 +0000

    Updated Copyright in license header.
    	* preferences-dialog.c:
    	* tab.h:
    	* tab.c:
    	* application.c:
    	* application.h:
    	* window.c:
    	* window.h:
    	* translation-memory-ui.c:
    	* translation-memory-ui.h:
    	* prefs-manager.c:
    	* prefs-manager.h:
    	* msg.h:
    	* msg.c:
    	* profile-dialog.c:
    	* profile-dialog.h:
    	* profile.h:
    	* profile.c:
    	* utils.c:
    	* utils.h:
    	* po.c:
    	* po.h:
    	* assistant.c:
    	* header.h:
    	* header.c:
    
    	Updated Copyright in license header.
    
    svn path=/trunk/; revision=3785

--- src/utils.c
+++ src/utils.c
@@ -1,23 +1,30 @@
 /*
- * (C) 2001-2007 	Fatih Demir <kabalak@kabalak.net>
- * 			Ignacio Casal <nacho.resa@gmail.com>
- * 			Paolo Maggi 
+ * (C) 2001-2007 Fatih Demir <kabalak@kabalak.net>
+ *               Ignacio Casal <nacho.resa@gmail.com>
+ * 		 Paolo Maggi
+ *     2008      Igalia
  *
- * 	Based in gedit utils funcs.
+ * Based in gedit utils funcs
  *
  * gtranslator is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or   
- *    (at your option) any later version.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.
  *    
  * gtranslator is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- *    GNU General Public License for more details.
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors:
+ *   Fatih Demir <kabalak@kabalak.net>
+ *   Pablo Sanxiao <psanxiao@gmail.com>
+ *   Ignacio Casal <nacho.resa@gmail.com>
+ *   Paolo Maggi 
  *
  */
 
