a63a3f9052e1a5d6d97b30d329f39b443b1bc9c1
Ignacio Casal Quinteiro <icq@gnome.org>
2009-08-17 23:10:47 +0200

    Forgot to add the file.

---
+++ src/actions-documents.c
@@ -0,0 +1,40 @@
+/*
+ * actions-documents.c
+ * This file is part of gtranslator
+ *
+ * Copyright (C) 2009 - Ignacio Casal Quinteiro
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANMSGILITY or FITNESS FOR A PARTICULAR PURMSGSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "actions.h"
+
+void
+gtranslator_actions_documents_next_document (GtkAction  * action,
+					     GtranslatorWindow *window)
+{
+	GtranslatorNotebook *notebook;
+	notebook = gtranslator_window_get_notebook (window);
+	gtk_notebook_next_page (GTK_NOTEBOOK (notebook));
+}
+
+void
+gtranslator_actions_documents_previous_document (GtkAction  * action,
+						 GtranslatorWindow *window)
+{
+	GtranslatorNotebook *notebook;
+	notebook = gtranslator_window_get_notebook (window);
+	gtk_notebook_prev_page (GTK_NOTEBOOK (notebook));
+}
