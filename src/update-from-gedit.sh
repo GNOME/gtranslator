cf4a28e3dc50cf897dcecb99de269df9941bc450
Ignacio Casal Quinteiro <icq@gnome.org>
2010-01-27 19:15:38 +0100

    Do not update message area anymore

--- src/update-from-gedit.sh
+++ src/update-from-gedit.sh
@@ -5,9 +5,7 @@
 
 SVN_URI=http://svn.gnome.org/svn/gedit/trunk/gedit
 FILES="gedit-history-entry.h \
-       gedit-history-entry.c \
-       gedit-message-area.h \
-       gedit-message-area.c"
+       gedit-history-entry.c"
 
 echo "Obtaining latest version of the sources"
 for FILE in $FILES
@@ -18,7 +16,6 @@ done
 sed_it () {
 	sed \
 	-e 's/gedit-history-entry/history-entry/g' \
-	-e 's/gedit-message-area/message-area/g' \
 	-e 's/gedit-window.h/window.h/g' \
 	-e 's/#include "gedit-debug.h"/\/\/#include "gedit-debug.h"/g' \
 	-e 's/gedit/gtranslator/g' \
@@ -29,11 +26,7 @@ sed_it () {
 
 sed_it gedit-history-entry.h > history-entry.h
 sed_it gedit-history-entry.c > history-entry.c
-sed_it gedit-message-area.c > message-area.c
-sed_it gedit-message-area.h > message-area.h
 
-rm gedit-message-area.h
-rm gedit-message-area.c
 rm gedit-history-entry.c
 rm gedit-history-entry.h
 
