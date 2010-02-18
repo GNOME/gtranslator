c0ec13e8eee5f37468e89abe4c59f13c612fd5df
Ignacio Casal Quinteiro <icq@gnome.org>
2010-02-18 22:09:17 +0100

    Fix the update script.

--- src/update-from-gedit.sh
+++ src/update-from-gedit.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
 FILES="gedit-history-entry.h \
-       gedit-history-entry.c"
+       gedit-history-entry.c \
+       gedit-status-combo-box.h \
+       gedit-status-combo-box.c"
 
 sed_it () {
 	sed \
-	-e 's/gedit-history-entry/history-entry/g' \
-	-e 's/gedit-window.h/window.h/g' \
+	-e 's/gedit-history-entry/gtr-history-entry/g' \
+	-e 's/gedit-window.h/gtr-window.h/g' \
 	-e 's/#include "gedit-debug.h"/\/\/#include "gedit-debug.h"/g' \
-	-e 's/gedit/gtranslator/g' \
-	-e 's/Gedit/Gtranslator/g' \
+	-e 's/gedit/gtr/g' \
+	-e 's/Gedit/Gtr/g' \
 	-e 's/GEDIT/GTR/g' \
 	$1
 }
 
-sed_it gedit-history-entry.h > history-entry.h
-sed_it gedit-history-entry.c > history-entry.c
-
-rm gedit-history-entry.c
-rm gedit-history-entry.h
-
+for i in $FILES
+do
+	if [ -f $i ]; then
+		sed_it $i > `echo $i | sed -e s/gedit/gtr/g`
+		rm $i
+	fi
+done
