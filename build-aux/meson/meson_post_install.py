3e025046152a16eb0c55295fb63d177a67989242
Iñigo Martínez <inigomartinez@gmail.com>
2018-09-19 12:20:08 +0200

    build: Various meson related improvements
    meson build system has received several improvements. The list of
    improvements is as follows:
    
    - The prefix used for different variables has been changed to `gtr`.
    - Indentation has been changed to that most commonly used.
    - Unnecessary `meson.build` files have been removed.
    - GTK documentation generation has been fixed.
    - `schemadir` location is now checked from GIO's pkg-config file
      when available.
    - `profile` has been removed and debug builds are considered now
      development builds.
    - `documentation` option has been renamed to `gtk_doc` to be
      consistent with different GNOME packages using the same option.
    - meson post install script does not build paths based on
      installation prefix because these might vary.

--- build-aux/meson/meson_post_install.py
+++ build-aux/meson/meson_post_install.py
@@ -2,14 +2,14 @@
 
 import os
 import subprocess
-
-install_prefix = os.environ['MESON_INSTALL_PREFIX']
-icondir = os.path.join(install_prefix, 'share', 'icons', 'hicolor')
-schemadir = os.path.join(install_prefix, 'share', 'glib-2.0', 'schemas')
+import sys
 
 if not os.environ.get('DESTDIR'):
-  print('Update icon cache...')
-  subprocess.call(['gtk-update-icon-cache', '-f', '-t', icondir])
+    icondir = os.path.join(sys.argv[1], 'icons', 'hicolor')
+    schemasdir = sys.argv[2]
 
-  print('Compiling gsettings schemas...')
-  subprocess.call(['glib-compile-schemas', schemadir])
+    print('Update icon cache...')
+    subprocess.call(['gtk-update-icon-cache', '-f', '-t', icondir])
+
+    print('Compiling gsettings schemas...')
+    subprocess.call(['glib-compile-schemas', schemasdir])
