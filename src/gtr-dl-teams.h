58c91f9ffa993980fd5997c3a0b3993a9a0acdb9
Teja Cetinski <teja@cetinski.eu>
2019-01-09 10:42:31 +0100

    dl-integration: Change copyright info

--- src/gtr-dl-teams.h
+++ src/gtr-dl-teams.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018  Daniel Garcia Moreno <danigm@gnome.org>
+ * Copyright (C) 2018  Teja Cetinski <teja@cetinski.eu>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
