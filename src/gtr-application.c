2106851818b1abdc973f62c1a6c83a072a27b059
Sabri Ünal <libreajans@gmail.com>
2023-01-19 17:49:16 +0000

    application: Use standart shortcuts

--- src/gtr-application.c
+++ src/gtr-application.c
@@ -695,9 +695,9 @@ gtr_application_startup (GApplication *application)
   set_kb (application, "app.save", "<Ctrl>s");
   set_kb (application, "app.saveas", "<Ctrl><Shift>s");
   set_kb (application, "app.upload_file", "<Ctrl>b");
-  set_kb (application, "app.preferences", "<Ctrl>p");
-  set_kb (application, "app.shortcuts", "<Ctrl><Shift>question");
-  set_kb (application, "app.help", "<Primary>h");
+  set_kb (application, "app.preferences", "<Ctrl>comma");
+  set_kb (application, "app.shortcuts", "<Ctrl>question");
+  set_kb (application, "app.help", "F1");
   set_kb (application, "app.quit", "<Primary>q");
 
   set_kb (application, "app.undo", "<Ctrl>z");
