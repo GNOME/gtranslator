984757daa76c95288e1ed1f670c43984e825af9c
Maximiliano Sandoval <msandova@gnome.org>
2025-08-28 21:50:20 +0200

    Remove codeview
    This is not in use. The only code that affected the app was in window.c.
    Due to the sandbox it is not possible to use this feature.

--- src/gtr-window.c
+++ src/gtr-window.c
@@ -43,8 +43,6 @@
 #include "translation-memory/gtr-translation-memory-dialog.h"
 #include "translation-memory/gda/gtr-gda.h"
 
-#include "codeview/gtr-codeview.h"
-
 #include <adwaita.h>
 #include <glib.h>
 #include <glib-object.h>
@@ -67,8 +65,6 @@ typedef struct
   GSettings *ui_settings;
   GSettings *tm_settings;
   GtrTranslationMemory *translation_memory;
-
-  GtrCodeView *codeview;
 
   GtkWidget *projects;
   GtkWidget *dlteams;
@@ -296,7 +292,6 @@ gtr_window_dispose (GObject * object)
   g_clear_object (&priv->prof_manager);
   g_clear_object (&priv->translation_memory);
   g_clear_object (&priv->tm_settings);
-  g_clear_object (&priv->codeview);
 
   G_OBJECT_CLASS (gtr_window_parent_class)->dispose (object);
 }
@@ -453,9 +448,6 @@ gtr_window_create_tab (GtrWindow * window, GtrPo * po)
       adw_navigation_page_set_child (page, GTK_WIDGET (tab));
       adw_navigation_view_pop_to_tag (priv->navigation_view, "poeditor");
     }
-
-  // code view
-  priv->codeview = gtr_code_view_new (window);
 
   g_signal_connect_after (po,
                           "notify::state",
